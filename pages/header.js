ce20ad9dd644eb3b5b19d4a6dc800f3f56899fb4
mauricioc08 <mauricio-cassiano@hotmail.com>
2022-09-02 15:38:20 -0300

    solved fixed vercel

--- pages/header.js
+++ pages/header.js
@@ -1,14 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Header(props) {
   return (
     <>
     <div className='border-white text-center border-2 mauricio'>
     <h1>Menu Header {props.quem}</h1>
-    <a href="/portifolio">Portifolio</a><br />
-    <a href="/">Home</a>
+    <Link href="/portifolio">Portifolio</Link><br />
+    <Link href="/">Home</Link>
     </div>
     </>
   )
