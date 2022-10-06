fa75ea9059bcf38837200d957823b80c13325ab7
mauricioc08 <mauricio-cassiano@hotmail.com>
2022-10-06 19:24:59 -0300

    implementing images and styles

--- pages/header.js
+++ pages/header.js
@@ -8,7 +8,7 @@ export default function Header(props) {
     <>
     <body className='w-screen'>
     
-    <header className='flex items-center justify-between px-40 py-20 max-w-1200 m-0'>
+    <header className='flex items-center justify-between px-5 py-10 max-w-7xl m-0 mx-auto'>
      <h1 className='text-5xl font-bold p-0 m-0 cursor-pointer'>Mauricio Cassiano</h1>
      <nav >
       <ul className='flex flex-wrap gap-10 justify-center'>
@@ -19,6 +19,14 @@ export default function Header(props) {
      </nav>
     
     </header>
+
+    <main className='grid grid-cols-3 items-center gap-28 max-w-7xl m-0 mx-auto px-5 py-10'> 
+      <img className='rounded-bl-lg' src="./img/minha_foto.jpg" alt="foto perfil"/>
+      <div className='grid col-start-2'>
+        <h1 className='text-7xl leading-tight font-bold mb-8 relative'>Desenvolvedor <br/>Front-End</h1>
+        <p className='text-2xl'>Localizado em Guarulhos-SP</p>
+      </div>
+    </main>
 
     
 
