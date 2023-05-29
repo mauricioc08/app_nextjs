529bc507ce9a6303cc544fd01161135f64c31399
mauricioc08 <mauricio-cassiano@hotmail.com>
2023-05-29 18:08:15 -0300

    add news styleds

--- pages/footer.js
+++ pages/footer.js
@@ -1,25 +1,45 @@
-import Head from 'next/head'
-import Link from 'next/link'
-
 export default function Footer(props) {
   return (
-    
     <footer className="footer" id="contato">
       <div className="footer-container">
         <p className="footer-texto">
-          Estou disponível para novos projetos no momento. Entre em contato comigo e marcamos uma conversa 👋
+          Estou disponível para novos projetos no momento. Entre em contato
+          comigo e marcamos uma conversa 👋
         </p>
         <ul className="footer-contato">
           <li> mauricio-cassiano@hotmail.com</li>
-          <li><a href="https://api.whatsapp.com/send?1=pt_BR&phone=5511945396825" target="_blank" rel="noreferrer" >Whatsapp</a></li>
-          <li><a href="https://www.linkedin.com/in/mauricio-cassiano-4186b0164/" target="_blank" rel="noreferrer" >Linkedin</a></li>
-          <li><a href="https://github.com/mauricioc08" target="_blank" rel="noreferrer" >GitHub</a></li>
+          <li>
+            <a
+              href="https://api.whatsapp.com/send?1=pt_BR&phone=5511945396825"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Whatsapp
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/mauricio-cassiano-4186b0164/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Linkedin
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/mauricioc08"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+          </li>
         </ul>
         <p className="footer-copy">
           Mauricio Cassiano © Alguns direitos reservados.
         </p>
       </div>
     </footer>
-
-  )
+  );
 }
