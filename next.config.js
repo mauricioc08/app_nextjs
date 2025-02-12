6c701da3f3a9c70f215baacd4efaca73960a73ca
mauricioc08 <mauricio-cassiano@hotmail.com>
2025-02-12 19:37:59 -0300

    adjust images next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,24 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'app-nextjs-theta.vercel.app',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost:3000',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
